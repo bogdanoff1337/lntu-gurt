261cdd99a60ede2e640c9dee26102d191738c23d
bogdanoff1337 <bogdanpanasuk437@gmail.com>
2024-03-30 11:31:06 +0200

    Add access table to admin table

---
+++ app/Filament/Resources/AccessResource.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AccessResource\Pages;
+use App\Filament\Resources\AccessResource\RelationManagers;
+use App\Models\AccessToRegister;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+class AccessResource extends Resource
+{
+    protected static $title = 'Доступи';
+    protected static ?string $model = AccessToRegister::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('email')
+                    ->label('Емейл')
+                    ->required(),
+                Select::make('access')
+                    ->options([
+                        1 => 'Так',
+                        0 => 'Ні',
+                    ])
+                    ->label('Дозвіл')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Емейл')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\SelectColumn::make('access')
+                    ->options([
+                        1 => 'Так',
+                        0 => 'Ні',
+                    ])
+                    ->label('Дозвіл')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAccesses::route('/'),
+            'create' => Pages\CreateAccess::route('/create'),
+            'edit' => Pages\EditAccess::route('/{record}/edit'),
+        ];
+    }
+}
