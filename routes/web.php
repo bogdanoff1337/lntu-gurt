3a0978d1752da99ed8f90fa614611f2e086d9783
vladyslav-my <vladisl13777@gmail.com>
2024-04-08 22:09:56 +0300

    Resolved pull

--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/admin/{any?}', fn () => view("control"))->where('any', '.*');
 */
 
-<<<<<<< HEAD
-Route::get('admin/{any?}', function ($any = null) {
-    return view("control");
-})->where('any', '.*');
-
-Route::get('{any?}', function ($any = null) {
-    return view("main");
-})->where('any', '^(?!admin).*$');
-=======
 Route::get('{any?}', fn () => view("main"))->where('any', '.*');
->>>>>>> d95c75f6e8549cab0456f6ccb656300b2cab7abb
