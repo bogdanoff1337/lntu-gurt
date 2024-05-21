149593c41800eab4f68252a69f026d2648de677f
vladyslav-my <vladisl13777@gmail.com>
2024-05-21 14:05:40 +0300

    uncommented in blade

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,8 +6,8 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
    <title>App</title>
    {{ Vite::useBuildDirectory('/main') }}
-   {{-- @viteReactRefresh --}}
-   {{-- @vite('src/main.tsx') --}}
+   @viteReactRefresh
+   @vite('src/main.tsx')
   </head>
   <body>
     <div id="root"></div>
