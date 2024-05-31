0afbec7442ad9301749518338abf3ce541fcb9f0
vladyslav-my <vladisl13777@gmail.com>
2024-05-31 20:52:05 +0300

    changes

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://lntu-gurt.onrender.com'],
+    'allowed_origins' => [env('APP_URL')],
 
     'allowed_origins_patterns' => [''],
 
