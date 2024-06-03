587a1649333a18279c0209cd13e02a5b768ec080
Vladislav <vladisl13777@gmail.com>
2024-06-03 23:50:57 +0300

    Refactored routes moddlewares

--- resources/main/src/app/providers/router/ui/RequireAuth.tsx
+++ resources/main/src/app/providers/router/ui/RequireAuth.tsx
@@ -5,17 +5,31 @@ import { entityAuthSelectors } from "@/entities/Auth";
 import { getLoginRoutePath, getMainRoutePath, getNonActiveRoutePath } from "@/shared/config/routes/path";
 import { Middleware } from "../routes/routes";
 
+const useAuthNavigation = (middleware: Middleware, authData: any, location: any) => {
+	if (middleware.includes(Middleware.AUTH)) {
+		if (!authData) {
+			return <Navigate replace state={{ from: location }} to={getLoginRoutePath()} />;
+		}
+		if (!middleware.includes(Middleware.NO_VERIFY) && authData && !authData.verified) {
+			return <Navigate replace state={{ from: location }} to={getNonActiveRoutePath()} />;
+		}
+	}
+
+	if ((middleware.includes(Middleware.NO_AUTH) && authData)
+      || (middleware.includes(Middleware.NO_VERIFY) && authData?.verified)) {
+		return <Navigate replace state={{ from: location }} to={getMainRoutePath()} />;
+	}
+
+	return null;
+};
+
 export const RequireAuth = ({ children, middleware }: { children: ReactNode, middleware: Middleware }) => {
 	const authData = useSelector(entityAuthSelectors.getData);
 	const location = useLocation();
 
-	if (middleware.includes(Middleware.AUTH) && !authData) {
-		return <Navigate replace state={{ from: location }} to={getLoginRoutePath()} />;
-	} else if (middleware.includes(Middleware.AUTH) && !middleware.includes(Middleware.NO_VERIFY) && authData && !authData.verified) {
-		return <Navigate replace state={{ from: location }} to={getNonActiveRoutePath()} />;
-	} else if (middleware.includes(Middleware.NO_AUTH) && authData
-	|| middleware.includes(Middleware.NO_VERIFY) && authData && authData?.verified) {
-		return <Navigate replace state={{ from: location }} to={getMainRoutePath()} />;
+	const navigation = useAuthNavigation(middleware, authData, location);
+	if (navigation) {
+		return navigation;
 	}
 
 	return children;
