e4581e6c56f3b185a41bd39661fcf321a1678e3f
Vladislav <vladisl13777@gmail.com>
2024-06-05 05:47:41 +0300

    Refactoring plop

--- resources/main/plopfile.js
+++ resources/main/plopfile.js
@@ -3,78 +3,92 @@ import { camelCase, pascalCase } from "change-case";
 export default function (plop) {
   const layers = ['entity', 'feature', 'page'];
 
+  // Registering helpers for case conversion
+  plop.setHelper('pascalCase', (text) => pascalCase(text));
+  plop.setHelper('camelCase', (text) => camelCase(text));
 
-  plop.setHelper('pascalCase', (text) => pascalCase(text))
-  plop.setHelper('camelCase', (text) => camelCase(text))
+  // Helper function to determine the folder path based on the layer
+  const getLayerPath = (layer) => {
+    switch (layer) {
+      case 'entity':
+        return 'entities';
+      case 'feature':
+        return 'features';
+      case 'page':
+        return 'pages';
+      default:
+        throw new Error(`Unknown layer: ${layer}`);
+    }
+  };
 
+  // Template paths
+  const templates = {
+    slice: 'plop-templates/Slice/model/slices/layerSliceSlice.ts.hbs',
+    schema: 'plop-templates/Slice/model/types/LayerSliceSchema.ts.hbs',
+    api: 'plop-templates/Slice/model/services/sliceApi.ts.hbs',
+    uiComponent: 'plop-templates/Slice/ui/Slice/Slice.tsx.hbs',
+    uiStyles: 'plop-templates/Slice/ui/Slice/Slice.module.scss.hbs',
+    selectors: 'plop-templates/Slice/model/selectors/index.ts.hbs',
+    index: 'plop-templates/Slice/index.ts.hbs'
+  };
+
+  // Generator for creating a new slice
   plop.setGenerator('slice', {
-      description: 'Create new slice',
-      prompts: [
-          {
-              type: 'list',
-              name: 'layer',
-              message: 'Select layer:',
-              choices: layers
-          },
-          {
-              type: 'input',
-              name: 'slice',
-              message: 'Write name of slice:'
-          },
-      ],
-      actions: function(data) {
-        const layerPath = () => {
-            if (data.layer === 'entity') {
-                return 'entities';
-            }
-            
-            if (data.layer === 'feature') {
-                return 'features';
-            }
-            
-            if (data.layer === 'page') {
-                return 'pages';
-            }
-        }
+    description: 'Create new slice',
+    prompts: [
+      {
+        type: 'list',
+        name: 'layer',
+        message: 'Select layer:',
+        choices: layers
+      },
+      {
+        type: 'input',
+        name: 'slice',
+        message: 'Write name of slice:'
+      },
+    ],
+    actions: function(data) {
+      const layerPath = getLayerPath(data.layer);
+      const basePath = `src/${layerPath}/${pascalCase(data.slice)}`;
 
-          const path = `src/${layerPath()}/${pascalCase(data.slice)}`;
-          return [
-              {
-                type: 'add',
-                path: `${path}/model/slice/{{layer}}{{slice}}Slice.ts`,
-                templateFile: 'plop-templates/Slice/model/slices/layerSliceSlice.ts.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/model/types/{{pascalCase layer}}{{pascalCase slice}}Schema.ts`,
-                templateFile: 'plop-templates/Slice/model/types/LayerSliceSchema.ts.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/model/services/{{camelCase slice}}Api.ts`,
-                templateFile: 'plop-templates/Slice/model/services/sliceApi.ts.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/ui/{{pascalCase slice}}/{{pascalCase slice}}.tsx`,
-                templateFile: 'plop-templates/Slice/ui/Slice/Slice.tsx.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/ui/{{pascalCase slice}}/{{pascalCase slice}}.module.scss`,
-                templateFile: 'plop-templates/Slice/ui/Slice/Slice.module.scss.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/model/selectors/index.ts`,
-                templateFile: 'plop-templates/Slice/model/selectors/index.ts.hbs'
-              },
-              {
-                type: 'add',
-                path: `${path}/index.ts`,
-                templateFile: 'plop-templates/Slice/index.ts.hbs'
-              },
-          ];
-      }
+      return [
+        {
+          type: 'add',
+          path: `${basePath}/model/slices/{{layer}}{{slice}}Slice.ts`,
+          templateFile: templates.slice
+        },
+        {
+          type: 'add',
+          path: `${basePath}/model/types/{{pascalCase layer}}{{pascalCase slice}}Schema.ts`,
+          templateFile: templates.schema
+        },
+        {
+          type: 'add',
+          path: `${basePath}/model/services/{{camelCase slice}}Api.ts`,
+          templateFile: templates.api
+        },
+        {
+          type: 'add',
+          path: `${basePath}/ui/{{pascalCase slice}}/{{pascalCase slice}}.tsx`,
+          templateFile: templates.uiComponent
+        },
+        {
+          type: 'add',
+          path: `${basePath}/ui/{{pascalCase slice}}/{{pascalCase slice}}.module.scss`,
+          templateFile: templates.uiStyles
+        },
+        {
+          type: 'add',
+          path: `${basePath}/model/selectors/index.ts`,
+          templateFile: templates.selectors
+        },
+        {
+          type: 'add',
+          path: `${basePath}/index.ts`,
+          templateFile: templates.index
+        },
+      ];
+    }
   });
-};
+}
