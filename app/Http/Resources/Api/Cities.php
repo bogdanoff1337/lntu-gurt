6da9e000ae9be85c267648872b2b1aaf66451733
bogdanoff1337 <bogdanpanasuk437@gmail.com>
2024-06-13 21:25:57 +0300

    fix city3

--- app/Http/Resources/Api/Cities.php
+++ app/Http/Resources/Api/Cities.php
@@ -18,7 +18,7 @@ class Cities extends JsonResource
             'id' => $this->id,
             'slug' =>
                 mb_convert_case($this->name, MB_CASE_TITLE, "UTF-8") . ', ' .
-                mb_convert_case($this->region, MB_CASE_TITLE, "UTF-8") . '' .
+                mb_convert_case($this->region, MB_CASE_TITLE, "UTF-8") . ' ' .
                 mb_convert_case($this->community, MB_CASE_TITLE, "UTF-8") . ' р-н., ' ,
         ];
 
